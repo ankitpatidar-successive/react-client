dac8c91a8da99de3046607fb7b8f551ea6f96902
patidar-successive <ankit.patidar@successive.tech>
2021-11-03 16:47:39 +0530

    modified

--- patterns/diamond.js
+++ patterns/diamond.js
@@ -1,27 +1,25 @@
 let n = 5;
-let string = "";
-// Upside pyramid
-for (let i = 1; i <= n; i++) {
-  // printing spaces
-  for (let j = n; j > i; j--) {
-    string += " ";
-  }
-  // printing star
-  for (let k = 0; k < i * 2 - 1; k++) {
-    string += "*";
-  }
-  string += "\n";
-}
-// downside pyramid
-for (let i = 1; i <= n - 1; i++) {
-  // printing spaces
-  for (let j = 0; j < i; j++) {
-    string += " ";
-  }
-  // printing star
-  for (let k = (n - i) * 2 - 1; k > 0; k--) {
-    string += "*";
-  }
-  string += "\n";
-}
-console.log(string);
+    let string = "";
+    // Upside pyramid
+    for (let i = 1; i <= n; i++) {
+      for (let j = n; j > i; j--) {
+        string += " ";
+      }
+       // printing star
+      for (let k = 0; k < i; k++) {
+        string += "* ";
+      }
+      string += "\n";
+    }
+    // downside pyramid
+    for (let i = 0; i <= n-1 ; i++) {
+      for (let j = 0; j < i; j++) {
+        string += " ";
+      }
+      // printing star
+      for (let k = (n - i); k > 0; k--) {
+        string += "* ";
+      }
+      string += "\n";
+    }
+    console.log(string);
