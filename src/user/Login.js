274768c2043db718a8c745473f9d0cf9fa7a3fc2
patidar-successive <ankit.patidar@successive.tech>
2022-01-10 09:52:02 +0530

    day 1 task

--- src/user/Login.js
+++ src/user/Login.js
@@ -1,73 +1,102 @@
 import React from 'react';
+import './LoginCss.css'
 import { TextField,Button,Form,FormLayout} from '@shopify/polaris';
-import { useState , useCallback} from 'react';
-import './LoginCss.css';
-const  Login = () => {
-    const handleSubmit = useCallback((_event) => {
-        setemail('');
-        setpassword('');
-      }, []);
-    const [email,setemail] = useState("");
-    const [password,setpassword] = useState("");
-    const [allUser,setAllUser] = useState([]);
-    const handleChange = useCallback((newValue) => setemail(newValue), []);
-    const handleChangePass = useCallback((newValue) => setpassword(newValue), []);
-    const onSubmit = (e) =>{
-        e.preventDefault();
-        const newUser={email:email,password:password}
-        //window.alert(newUser.password);
-        setAllUser([...allUser,newUser]);
-        setemail("");
-        setpassword("");
-        window.alert(newUser.email +" " +  newUser.password);
-        //console.log(allUser);
-
+import { useState } from 'react';
+  const  Login = () => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
+  const emailValidation = () => {
+    var regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regex)){
+        return true;
     }
-  return(
-      <div className="main">
-      <Form onSubmit={handleSubmit}>
-          <FormLayout><h1 className="sub-main">Login Page</h1>
-     <TextField label="Email" autoComplete="off" align="left" type="email" value={email}
-      onChange={handleChange} className="email" />
-            <TextField label="Password" autoComplete="off" align="left" type="password"value={password}
-      onChange={handleChangePass} />
+    return false;
+}
+const passwordValidation = () => {
+  var passregex = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
+  if(password.match(passregex)){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+  const onSubmitt = () =>{
+    var res = emailValidation();
+    var pass = passwordValidation();
+    if(!res){
+      alert("email is not in proper format")
+    }
+    
+    else if (!pass){
+      alert("password is not in proper format")
+    }
+    else{
+      window.alert("Email:"+ email + "  Password:" + password);
+      setEmail('');
+      setPassword('');
+    }
+    }
+  
+  return( 
+    <div className = "main">
+    <h1 className = "heading">Welcome to my Login Page</h1>
+      <Form onSubmit = {onSubmitt}> 
+          <FormLayout>
+            <div className = "sub-main">
+     <TextField label="Email" autoComplete="off" align="left" type="email" value= {email}
+      onChange ={ (newValue) => setEmail(newValue)} />
+            <br/>
+            <TextField label="Password" autoComplete="off" align="left" type="password" value = {password}
+      onChange ={ (newValue) => setPassword(newValue)} />
+      </div>
       <div className="buttonCss">
-            <Button onClick={onSubmit} >Submit</Button>
+            <Button submit >Submit</Button>
             </div>
             </FormLayout>
         </Form>
-        </div>
+      </div>
   );
 
 }
 export default Login;
 
-// class Login extends React.Component {
-//     constructor(props){
-//       super(props);
-//       this.state={
-//       email:'',
-//       password:''
-//       }
-//      }
-//      handleSubmit(){
-//          console.log('hfkwekysdfgvard');
-//      }
-//     render() {
-//    return( 
-//       <Form onSubmit={this.handleSubmit}>
-//           <FormLayout>
-//      <TextField label="Email" autoComplete="off" align="left" type="email" value={this.email}
-//       onChange={(newValue) => this.setState({email:newValue})} />
-//             <br/>
-//             <TextField label="Password" autoComplete="off" align="left" type="password"value={this.password}
-//       onChange={(newValue) => this.setState({password:newValue})} />
-//             <br></br>
-//             <Button submit>Submit</Button>
-//             </FormLayout>
-//         </Form>
-//       );
-
-//       }
-//     }
-//     export default Login;
+/*
+This is Class component
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+    };
+    this.onSubmitt = this.onSubmitt.bind(this);
+  }
+  onSubmitt(event) {
+    window.alert("email:" + this.state.email)
+    this.setState({email:"",password:""});
+    event.preventDefault();
+  }
+  render(){
+  return( 
+    <div className = "form">
+    <h1 className = "heading">Welcome to my Login Page</h1>
+      <Form onSubmit = {this.onSubmitt}> 
+          <FormLayout>
+            <div className = "textfield">
+     <TextField label="Email" autoComplete="off" align="left" type="email" value= {this.state.email}
+      onChange = {(newValue) => this.setState({email:newValue})} />
+            <br/>
+            <TextField label="Password" autoComplete="off" align="left" type="password" value = {this.state.password}
+     onChange = {(newValue) => this.setState({password:newValue})} />
+      </div>
+      <div className="submit">
+            <Button submit >Submit</Button>
+            </div>
+            </FormLayout>
+        </Form>
+      </div>
+  );
+  }
+}
+*/
