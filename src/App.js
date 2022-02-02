e1bae7fd7fbeadccec22fff546ef277cc7eb3908
patidar-successive <ankit.patidar@successive.tech>
2022-02-02 14:53:37 +0530

    task completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import enTranslations from '@shopify/polaris/locales/en.json';
-import { AppProvider } from '@shopify/polaris';
-import Routers from "./route/router";
-const App = () => {
-  return (
-    <div>
-      <AppProvider className="sub-main" i18n={enTranslations}>
+// import React from "react";
+// import enTranslations from '@shopify/polaris/locales/en.json';
+// import { AppProvider } from '@shopify/polaris';
+// import Routers from "./route/router";
+// const App = () => {
+//   return (
+//     <div>
+//       <AppProvider className="sub-main" i18n={enTranslations}>
 
-        <Routers />
-      </AppProvider>
-    </div>
-  );
-}
-export default App;
+//         <Routers />
+//       </AppProvider>
+//     </div>
+//   );
+// }
+// export default App;
